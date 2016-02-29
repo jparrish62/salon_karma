2e8481cc1f69ece9f09d014ebcc584fc633cc768
Julian <parrishjulian62@gmail.com>
2016-02-29 17:16:12 -0500

    set up user model for roles

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "tonya@gmail.com"
+    password "12345678"
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,31 +1,35 @@
 require 'rails_helper'
 
 feature 'Sign in', :devise do
-    scenario 'user cannot sign in if not registered' do
-        def sign_in(email,password)
+    before do
+        @user = FactoryGirl.build(:user)
+        def sign_in(user)
             visit new_user_session_path
             fill_in "user_email", :with => user.email
             fill_in "user_password", :with => user.password
-            find('#login').click
+            click_on 'Log in'
         end
-        sign_in('phil@gmail.com','12345678')
-        expect(page).to have_content 'invalid email or password'
+    end
+    scenario 'user cannot sign in if not registered' do
+        @user = FactoryGirl.build(:user,email: nil, password: nil)
+        sign_in(@user)
+        expect(@user).not_to be_valid
     end
     scenario ' user can sign in with valid credentials' do
-        user = FactoryGirl.create(:user)
-        sign_in(user)
-        expect(page).to have_content "Signed in successfully."
+        @user = FactoryGirl.build(:user)
+        sign_in(@user)
+        expect(@user).to be_valid
     end
 
     scenario "user cannot sign in with invalid email" do
-        user = FactoryGirl.create(:user,email: nil)
-        sign_in(user)
-        expect(page).to have_content "invalid email or password"
+        @user = FactoryGirl.build(:user,email: nil)
+        sign_in(@user)
+        expect(@user).not_to be_valid
     end
     scenario "user cannot sign in with invalid password" do
-        user = FactoryGirl.create(:user,password: nil)
-        sign_in(user)
-        expect(page).to have_content "invalid email or password"
+        @user = FactoryGirl.build(:user,password: nil)
+        sign_in(@user)
+        expect(@user).not_to be_valid "invalid email or password"
     end
 
 end
