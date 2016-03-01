86bbe63fe60de4343a4b4754aa7064003344c292
Julian <parrishjulian62@gmail.com>
2016-03-01 14:30:38 -0500

    added update and destroy actions

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,25 @@
 class UserPolicy
     attr_reader :current_user, :model
 
-    def initialize
+    def initialize(current_user, model)
         @current_user = current_user
         @user = model
     end
 
     def index?
         @current_user.admin?
+    end
+
+    def show?
+        @current_user.admin? || @current_user == @user
+    end
+
+    def update?
+        @current_user.admin?
+    end
+
+    def destroy
+        return false if @current_user == @user
+        @current.admin? 
     end
 end
